6629f1b4be4640a30e307a295c36baee39cc5571
Joshua Pianto <p21jpianto@fairfieldprep.org>
2020-12-09 17:22:31 -0500

    hw 10

--- hw10-1-3.py
+++ hw10-1-3.py
@@ -5,10 +5,10 @@
 
 def thermostat():
     f_temp = input("Tempature in fahrenheit")
-    c_temp = (float(f_temp) - 32) * 5 / 932
+    c_temp = (float(f_temp) - 32) * 5 / 9
     print(c_temp)
 
 
 print(thermostat())
 
-# Plug in 50, you get .0965; Plug in 90 ypi get 0.25751; Plug in 60 = 0.15
+
